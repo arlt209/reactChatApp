6da227790698a60c0d7c6869fd006f0dc4eada41
arlt209 <nickarlt@gmail.com>
2016-09-21 02:54:14 -0700

    first commit of test socket branch,    settingup server and socket.io

--- chatComponent.js
+++ chatComponent.js
@@ -2,14 +2,34 @@
  * Created by arlt on 9/20/16.
  */
 var ChatApp = React.createClass({
+
+  getInitialState: function() {
+    return {
+      messages: [],
+      socket: window.io('http://localhost:3000')
+    }
+  },
+
+  componentDidMount: function(){
+    this.state.socket.emit.on("new-message", function(msg){
+      this.setState({ messages: this.state.messages.push(msg) })
+    });
+  },
+  // render function for ChatApp
   render: function(){
+  var self = this;
+
     return(
       <div>
-        This will be The Chat App.
+        <ul>
+
+        </ul>
+        <input id="message" type="text"><button onClick={ () => self.submitMessage() } ></button>
       </div>
     )
   }
-});
+      });
+
 
 ReactDOM.render(
   <ChatApp />,
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  */
 var express = require('express');
 var app = express();
-var http: = require('http').Server(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
@@ -11,8 +11,12 @@ io.on('connection', function(socket){
   socket.on("new-message", function(msg){
     console.log(msg);
     io.emit("receive-message", msg);
-  });
-  http.listen('3000', function(){
-    console.log("we are connected")
   })
+  socket.on("test", function(){
+    console.log("Mounted")
+  })
+});
+
+http.listen('3000', function(){
+  console.log("we are connected")
 });
