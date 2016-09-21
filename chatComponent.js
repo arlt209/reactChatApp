dbe6bcb55d490dae77be451339d795a3792cc52e
arlt209 <nickarlt@gmail.com>
2016-09-21 15:36:15 -0700

    Implemented Send/ recieve and select UserName functions
    created new functions for creating a username, created event handler for send button. 
    solved server posting no response error.   
    can now chat from multiple users on multiple browsers.   tested on safari, chrome and firefox.

--- chatComponent.js
+++ chatComponent.js
@@ -3,39 +3,63 @@
  */
 var ChatApp = React.createClass({
 
-  // state //
+  // initial state / state //
   getInitialState: function() {
     return {
       messages: [],
-      socket: window.io('http://localhost:3000')
+      socket: window.io('http://localhost:3000'),
+      user: undefined
     }
   },
 
+  // on componentDidMount will push messages to messages array stored in state//
+  componentDidMount: function() {
+    var self = this;
+    this.state.socket.on("receive-message", function (msg){
+      var messages = self.state.messages;
+          messages.push(msg);
+      self.setState({messages: messages});
+      console.log(self.state.messages);
+    });
+  },
+
+
+
   submitMessage: function(){
-    var message = document.getElementById('message').value;
+    var body = document.getElementById('message').value;
+    var message = {
+          body: body,
+          user: this.state.user || "guest"
+    };
     this.state.socket.emit("new-message", message);
     console.log(message);
   },
 
-   // on componentDidMount will push messages to messages state array //
-  componentDidMount: function() {
-    this.state.socket.on("new-message", function (msg){
-    this.setState( {messages: this.state.messages.push(msg)} )
-    });
+
+  pickUser: function(){
+    var user = document.getElementById("user").value;
+    this.setState({user: user})
   },
+
+
+
 
   // render function for ChatApp //
   render: function(){
-  var self = this;
 
-    return(
-      <div>
-        <ul>
-
-        </ul>
-        <input id="message" type="text" /> <button onClick={ () => self.submitMessage()}>Submit</button>
-      </div>
-    );
+    var self = this;
+    var messages = self.state.messages.map(function(msg){
+      return <li><strong>{msg.user}</strong><span> {msg.body}</span></li>
+    });
+      return(
+        <div>
+          <ul>
+            {messages}
+          </ul>
+          <input id="message" type="text" /> <button onClick={ () => self.submitMessage()}>Send</button><br/>
+          <input id="user" type="text" placeholder="choose username" /> <button onClick={() => self.pickUser()}>select a username</button>
+        </div>
+      );
   }
 });
 
